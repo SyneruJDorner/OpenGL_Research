8b08c8e5cd08c8d63918f0d3eaef8d9c1cc6630d
justindd1994 <justindd1994@gmail.com>
2020-01-07 21:19:34 +0200

    -Programmed a tranlation to the uniform Matrix 4 in the shader.

--- OpenGL_Research/OpenGL_Research.cpp
+++ OpenGL_Research/OpenGL_Research.cpp
@@ -4,7 +4,10 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <glm/mat4x4.hpp>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #define SUCCESS return 0
 #define ERROR return 1
@@ -12,7 +15,7 @@
 //Window dimenstions
 const GLint WIDTH = 800, HEIGHT = 600;
 
-GLuint VAO, VBO, shader, uniformXMove;
+GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
@@ -26,11 +29,11 @@ static const char* vShader = "											\n\
 																		\n\
 layout (location = 0) in vec3 pos;										\n\
 																		\n\
-uniform float xMove;													\n\
+uniform mat4 model;														\n\
 																		\n\
 void main()																\n\
 {																		\n\
-	gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, pos.z, 1.0);	\n\
+	gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);	\n\
 }																		\n\
 ";
 
@@ -136,7 +139,7 @@ int CompileShaders()
 	}
 	#pragma endregion
 
-	uniformXMove = glGetUniformLocation(shader, "xMove");
+	uniformModel = glGetUniformLocation(shader, "model");
 }
 
 int main()
@@ -211,14 +214,16 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shader);
-		glUniform1f(uniformXMove, triOffset);
-		
+
+		glm::mat4 model(1.0f);
+		model = glm::translate(model, glm::vec3(triOffset, triOffset, 0.0f));
+
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 		glUseProgram(0);
-
 
 		glfwSwapBuffers(window);
 	}
